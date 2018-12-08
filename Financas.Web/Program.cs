24aa18c50c53ce243e8ff1950c3932388ab4a8b6
Leonardo <leonardotreze@gmail.com>
2018-12-08 12:44:06 -0200

    Setup for Heroku

--- Financas.Web/Program.cs
+++ Financas.Web/Program.cs
@@ -14,7 +14,17 @@ namespace Financas.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
