773821f2b5b97a8c8e5f63f07429cead75966e16
Leonardo <leonardotreze@gmail.com>
2018-12-31 11:49:45 -0200

    scopo da diretiva transacao ok

--- wwwroot/js/app/home/transacao/transacao.js
+++ wwwroot/js/app/home/transacao/transacao.js
@@ -1,6 +1,9 @@
 finleo.directive('transacao', function () {
     return {
         restrict: 'E',
-        templateUrl: '/js/app/home/transacao/transacao.html'
+        templateUrl: '/js/app/home/transacao/transacao.html',
+        scope: {
+            tin: '='
+        }
     };
 });
